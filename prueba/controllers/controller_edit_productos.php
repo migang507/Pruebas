b3dcb8df330f3a467a582ba63717990e59d6a5b7
migang507 <83372163+migang507@users.noreply.github.com>
2021-04-28 20:29:24 -0500

    Update controller_edit_productos.php

--- prueba/controllers/controller_edit_productos.php
+++ prueba/controllers/controller_edit_productos.php
@@ -1,8 +1,12 @@
 <?php
-require_once( "models/model_productos.php");
-$prod=new model_productos();
+include "../db/db_con.php";
+$id=$_GET['id'];
+$crud=$_GET['crud'];
 
-$result=$prod->read_query("*","productos","");
+require_once( "../models/model_productos.php");
+$prod=new model_productos();
+$condicion="where id=".$id;
+$result=$prod->read_query("*","productos",$condicion);
 //var_dump($result);
-include "views/productos_editar.php";
+include "../views/productos_editar.php";
 ?>
